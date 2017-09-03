42eaadb8630d8dcf765b1d9b08811464b990bb0e
techcraftsman <31449571+techcraftsman@users.noreply.github.com>
2017-09-03 09:43:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello it is me\n');
+  res.end('It is me.\n');
 });
 
 server.listen(port, hostname, () => {
